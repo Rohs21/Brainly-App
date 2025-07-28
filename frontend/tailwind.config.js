a4d41a884a069f44ed9028861b8ca345ce0b5e01
Rohan Singh <rohans212004@gmail.com>
2025-07-28 18:13:23 +0530

    Dymanic Button Lib

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -5,8 +5,15 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        purple: {
+          300: "#bfb9eb",
+          500: "#240552",
+          600: "#07038a"
+        }
+      }
+    },
   },
   plugins: [],
 }
-
