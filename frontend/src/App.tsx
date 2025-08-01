0cd7b37bef23cc3944cbca388cd7092538f6f2a5
Rohan Singh <rohans212004@gmail.com>
2025-08-01 23:40:42 +0530

    Sidebar

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,18 @@
-import { useState } from 'react'
-import './App.css'
+import { BrowserRouter, Route, Routes } from "react-router-dom"
+import Dashboard from "./pages/Dashboard"
+import { Signin } from "./pages/Signin"
+import { Signup } from "./pages/Signup"
+import { Home } from "./components/Home/Home"
 
-function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-    </>
-  )
+const App = () => {
+  return <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Home/>} />
+      <Route path="/signup" element={<Signup />} />
+      <Route path="/signin" element={<Signin />} />
+      <Route path="/dashboard" element={<Dashboard />} />
+    </Routes>
+  </BrowserRouter>
 }
 
 export default App
